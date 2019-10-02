b13bc5cf24f6d428ed8bcbba26ca080280eac439
Your Name <you@example.com>
2019-10-02 16:47:03 -0700

    Changes made

--- app.js
+++ app.js
@@ -1,65 +1,116 @@
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// module.exports = app;
-//Load Modules
-const express =require('express');
+// Load modules
+const express = require('express');
+const expressLayouts= require('express-ejs-layouts');
 const path=require('path');
-const expressLayouts=require('express-ejs-layouts');
-const mongoose =require('mongoose');
-//Connect to Mongo uri exported from external file
-const keys= require('./config/keys');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 
-//
-const app=express();
+// Connect to MongoURI exported from external file
+const keys = require('./config/keys');
+const User = require('./models/user');
+require('./passport/google-passport');
 
 
-//Engine Setup.........
+// initialize application
+const app = express();
+
+
+// Express config
+ app.use(cookieParser());
+
+ app.use(bodyParser.urlencoded({
+     extended: false
+ }));
+
+ app.use(bodyParser.json());
+
+ app.use(session({
+     secret: 'keyboard cat',
+     resave: true,
+     saveUninitialized: true
+ }));
+
+
+ app.use(passport.initialize());
+ app.use(passport.session());
+
+ //Global variables for users
+
+ app.use((req,res,next)=>{
+ res.locals.user=req.user || null;
+ next();
+ });
+
+
+//Setting up the template
 app.set('views', path.join(__dirname, 'views')); 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-//setting up public folder
+// setup static file to serve css, javascript and images
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Connect to remote database
 
-mongoose.connect(keys.MongoURI,{useNewUrlParser: true, useUnifiedTopology: true})
-.then(function(){
-console.log("connected to remote database");
-}).catch((err)=>{
-console.log(err);
+// connect to remote database
+mongoose.promise=global.Promise;
+mongoose.connect(keys.MongoURI, {
+    useNewUrlParser: true,useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected to Remote Database....');
+}).catch((err) => {
+    console.log(err);
 });
-
-//Set environment Variable
+// set environment variable for port
 const port = process.env.PORT || 3000;
-
-
-
-app.get('/',(req,res)=>{
-res.render('main');
-});
-app.get('/about',(req,res)=>{
-res.render('about');
+// Handle routes
+app.get('/', (req, res) => {
+    res.render('main');
 });
 
-app.listen(port,()=>{
-    console.log(`Server is running by Nodemon on port ${port}`);
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+// GOOGLE AUTH ROUTE
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/'
+    }),
+    (req, res) => {
+        // Successful authentication, redirect home.
+        res.redirect('/profile');
+    });
+
+app.get('/profile', (req, res) => {
+   
+    User.findById({_id:req.user._id})
+    
+    .then((user)=>{
+        res.render('profile',{
+            user:user
+        });
+    })
+   
+});
+
+//Handle User Logout
+
+app.get('/logout',(req,res)=>{
+req.logout();
+res.redirect('/');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
