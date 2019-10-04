eb5ce0c7778f2580e84c42ed8ed4a19de67523ee
Your Name <you@example.com>
2019-10-03 18:56:16 -0700

    Config Keys added

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,6 +1,6 @@
 module.exports={
-    MongoURI :'mongodb+srv://shravan1:shravan12@cluster0-no1dp.gcp.mongodb.net/test1?retryWrites=true&w=majority',
-    GoogleClientID :'514541861996-vgtu3rhcqugkoude8uhf053k4r6jji9g.apps.googleusercontent.com',
-    GoogleClientSecret:'TOBHP7BXp7gELbMpKobw9T28'
+    MongoURI : process.env.MONGO_URI,
+    GoogleClientID :process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret:process.env.GOOGLE_CLIENT_SECRET
 
 }
